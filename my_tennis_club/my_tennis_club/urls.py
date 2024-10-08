761feeedf416c6aecb4f70a7025c87169098adf0
hymaybituin <152581582+hymaybituin@users.noreply.github.com>
2024-10-08 02:08:09 +0000

    add

--- my_tennis_club/my_tennis_club/urls.py
+++ my_tennis_club/my_tennis_club/urls.py
@@ -16,9 +16,8 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
 
 urlpatterns = [
-    path('', include('members.urls')),
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
 ]
